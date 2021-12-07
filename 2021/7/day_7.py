2b21790ada860172be5bf6e96f967ed959841ad3
samweston <sam@weston.net.nz>
2021-12-07 18:28:51 +1300

    Tidy up day 7 slightly.

--- 2021/7/day_7.py
+++ 2021/7/day_7.py
@@ -1,5 +1,4 @@
 
-
 def find_best_cost(high_costs):
     with open("2021/7/input", 'r') as f:
         positions = [ int(num) for num in f.readline().split(',') ]
@@ -7,29 +6,27 @@ def find_best_cost(high_costs):
     min_num = min(positions)
     max_num = max(positions)
 
+    costs = []
     if high_costs:
         costs = []
-        total_cost = 0
-        for i in range(0, max_num + 1):
-            costs.append(i + total_cost)
-            total_cost += i 
+        costs.append(0)
+        for i in range(1, max_num + 1):
+            costs.append(i + costs[i - 1])
+    else:
+        costs = range(0, max_num + 1)
 
     best_score = float('inf')
     best_position = -1
     for i in range(min_num, max_num):
         score = 0
         for num in positions:
-            if high_costs:
-                score += costs[abs(num - i)]
-            else:
-                score += abs(num - i)
+            score += costs[abs(num - i)]
         
         if score < best_score:
             best_score = score
             best_position = i
 
     return best_score
-
 
 
 if __name__ == "__main__":
